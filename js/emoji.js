970cc35e5c9cd5a7f6b8f6e6de6633cfadab56be
mudassirali007 <mudassiraliansari@gmail.com>
2023-10-08 09:48:43 +0500

    added emoji for text and canvas

--- js/emoji.js
+++ js/emoji.js
@@ -1,28 +1,30 @@
 /**
- * Define action to upload, drag & drop images into canvas
+ * Define action to add emoji into canvas
  */
 (function () {
-  var emoji = function (canvas) {
+  const emoji = function (canvas) {
     const _self = this;
-    const emojiDropDown = document.querySelector(`#emojiDropdown`);
-    emojiDropDown.addEventListener('change',  (e) => {
+    const emojiAddTextButton = document.querySelector(`#emoji-add-text`);
+    const emojiAddButton = document.querySelector(`#emoji-add`);
+    emojiAddButton.addEventListener('change',  (e) => {
+      const selectedEmojiValue = e.target.value;
+      _self.history.addToHistory();
+      const text = new fabric.Text(selectedEmojiValue);
+      _self.canvas.add(text);
+      _self.history.addToHistory();
+
+    })
+    emojiAddTextButton.addEventListener('change',  (e) => {
       const selectedEmojiValue = e.target.value;
       const activeObject = canvas.getActiveObject();
 
       if (activeObject && activeObject.type === 'textbox') {
-        // activeObject.text += selectedEmojiValue;
         activeObject.isEditing && activeObject.insertChars(selectedEmojiValue, null, activeObject.selectionStart, activeObject.selectionEnd);
         (!activeObject.isEditing) && (activeObject.text += selectedEmojiValue)
         canvas.renderAll();
-      } else {
-        _self.history.addToHistory();
-        const text = new fabric.Text(selectedEmojiValue);
-        _self.canvas.add(text);
-        _self.history.addToHistory();
       }
 
     })
-
 
   }
 
